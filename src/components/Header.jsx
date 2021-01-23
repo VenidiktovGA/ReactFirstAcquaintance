04b657e3c577b743975db51e306d1b3685fa4a2f
VenidiktovGA <wenter3@outlook.com>
2021-01-24 02:15:57 +0500

    Начало положено

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Header = () => {
+	return(
+		<header className='site-header'>
+			<img src='https://i.pinimg.com/originals/3a/79/40/3a79402af2dc2fe9958b47cc8d79be32.png' />
+		</header>
+	)
+}
