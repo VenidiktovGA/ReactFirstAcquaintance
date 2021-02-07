9cc582b7597b452294044af76e4d79d73b1aa340
GennadyVenidiktov <wenter3@outlook.com>
2021-02-08 01:48:48 +0500

    Rout'ы рботают
    Решил проблему не работающего роутинга тем что поместил ROut и Link под один тэг BrouserRouter

--- src/App.js
+++ src/App.js
@@ -3,14 +3,19 @@ import { Header } from './components/Header/Header';
 import { Nav } from './components/Nav/Nav';
 import { Main } from './components/Main/Main';
 import { Footer } from './components/Footer/Footer';
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className='site-wrapper'>
-			<Header/>
-			<Nav/>
-			<Main/>
-			<Footer/>
+			<Header />
+			<BrowserRouter>
+				<div className='rout-wrapper'>
+					<Nav />
+					<Main />
+				</div>
+			</BrowserRouter>
+			<Footer />
 		</div>
 	);
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,15 +2,15 @@ import React from 'react'
 import styles from './Main.module.css'
 import { Letters } from '../Letters/Letters'
 import { Numeral } from '../Numeral/Numeral'
-import { BrowserRouter, Route } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 export const Main = () => {
 	return (
-		<BrowserRouter>
 			<main className={styles.main}>
+				<Switch>
 				<Route path='/Letters' component={Letters} />
 				<Route path='/Numeral' component={Numeral} />
+				</Switch>
 			</main>
-		</BrowserRouter>
 	)
 }
