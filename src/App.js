63b4194ec0e07075d3044d0966d97c8df615ba77
GennadyVenidiktov <wenter3@outlook.com>
2021-02-09 03:17:25 +0500

    Revert "Rout'ы рботают"
    This reverts commit 9cc582b7597b452294044af76e4d79d73b1aa340.

--- src/App.js
+++ src/App.js
@@ -3,19 +3,14 @@ import { Header } from './components/Header/Header';
 import { Nav } from './components/Nav/Nav';
 import { Main } from './components/Main/Main';
 import { Footer } from './components/Footer/Footer';
-import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 	return (
 		<div className='site-wrapper'>
-			<Header />
-			<BrowserRouter>
-				<div className='rout-wrapper'>
-					<Nav />
-					<Main />
-				</div>
-			</BrowserRouter>
-			<Footer />
+			<Header/>
+			<Nav/>
+			<Main/>
+			<Footer/>
 		</div>
 	);
 }
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import { BrowserRouter, NavLink } from 'react-router-dom'
-import { Letters } from '../Letters/Letters'
 import styles from './Nav.module.css'
 
 export const Nav = (props) => {
 	return(
 		<nav className={styles.nav}>
 			<ul>
-				<li><NavLink to='/Letters'>Буквы</NavLink></li>
-				<li><NavLink to='/Numeral'>Цифры</NavLink></li>
+				<li><a href='/Letters'>Буквы</a></li>
+				<li><a href='/Numeral'>Цифры</a></li>
 				<li><a href='#'>3</a></li>
 				<li><a href='#'>4</a></li>
 			</ul>
